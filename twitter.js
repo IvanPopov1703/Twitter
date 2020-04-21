18956ba0e9448affd5342d50a327174a89a8abf6
Popov_Ivan <ivanwork137@yandex.ru>
2020-04-21 21:05:07 +0300

    Laba_1_JS

---
+++ twitter.js
@@ -0,0 +1,53 @@
+/*Лаба №3 про твитер. Попов И.В., 9 группа*/
+"use strict"
+
+function messageSplitting(textMess, sizeMess) {
+    let count = 0, prev = 0, next;
+    let len = String(textMess).length;
+    for (let i = 0; i < len; i++)
+    {
+        next = 0;
+        while (next < sizeMess && i < len)
+        {
+            if (textMess[i] == ' ')
+            {
+                prev = i;
+            }
+            i++;
+            next++;
+        }
+        if (prev == 0 && i == len)
+        {
+            count++;
+        }
+        else
+        {
+            if (prev != 0 || textMess[i] == ' ')
+            {
+                if (textMess[i] != ' ')
+                {
+                    i = prev;
+                }
+                count++;
+                prev = 0;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+    }
+    return count;
+}
+
+
+let textMessage = prompt("Введите текст сообщения:"); //Текст сообщения
+let sizeMessage = prompt("Введите максимальную длину одного сообщения:"); //Максимальная длина
+
+let result = messageSplitting(textMessage, sizeMessage);
+
+if (result === -1)
+    alert("Ошибка!");
+else
+    alert("Число постов = " + result);
