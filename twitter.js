63a364265ece0251e4baea8411af09a636a87d7a
Popov_Ivan <ivanwork137@yandex.ru>
2020-06-09 19:24:10 +0300

    Исправил лабу

--- twitter.js
+++ twitter.js
@@ -1,52 +1,53 @@
 /*Лаба №3 про твитер. Попов И.В., 9 группа*/
 "use strict"
 
-function messageSplitting(textMess, sizeMess) {
-    let count = 0, prev = 0, next;
-    let len = String(textMess).length;
-    for (let i = 0; i < len; i++)
-    {
-        next = 0;
-        while (next < sizeMess && i < len)
-        {
-            if (textMess[i] == ' ')
-            {
-                prev = i;
-            }
-            i++;
-            next++;
-        }
-        if (i == len)
-        {
-            count++;
-        }
-        else
-        {
-            if (prev != 0 || textMess == ' ')
-            {
-                if (textMess[i] != ' ')
-                {
-                    i = prev;
-                }
-                count++;
-                prev = 0;
-            }
-            else
-            {
-                return -1;
-            }
-        }
-
-    }
-    return count;
+//Функция для объединения элементов массива
+function len(arrWords) {
+    return arrWords.join(' ').length;
 }
+
+function messageSplitting(textMess, sizeMess) {
+    //Объединяем строку в массив, разбивая по пробелам
+    let arrWords = textMess.split(' ');
+    let msg = [], msgs = [];
+    let errFlag = 0;
+
+    msgs = arrWords.reduce((messages, word) => {
+        if ((errFlag == 0) && (word != "")) {
+            if (word.length > sizeMess) {
+                errFlag = -1;
+                return [];
+            }
+            let newmsg = msg.concat([word]);
+            if (len(newmsg) <= sizeMess) {
+                msg = newmsg;
+            } else {
+                messages.push(msg.join(' '));
+                msg = [word];
+            }
+        }
+        return messages;
+    }, []);
+
+    if (errFlag != 0)
+        return errFlag;
+
+    msgs.push(msg.join(' '));
+    return msgs;
+}
+
 
 let textMessage = prompt("Введите текст сообщения:"); //Текст сообщения
 let sizeMessage = prompt("Введите максимальную длину одного сообщения:"); //Максимальная длина
 
 let result = messageSplitting(textMessage, sizeMessage);
 
-if (result == -1)
+if (result == -1) {
     alert("Ошибка!");
-else
-    alert("Число постов = " + result);
+} else {
+    //Вывод в консоль
+    for (let s of result) {
+        console.log(s + "\n\n");
+    }
+    alert("Число постов = " + result.length);
+}
